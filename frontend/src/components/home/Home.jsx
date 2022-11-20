8fd95b98e8a620fd3a58661e8785205c8f1ae077
Daniel Chang <danielchang083@gmail.com>
2022-11-19 20:00:25 -0800

    Add home image, name, description, and social media icons.

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import "./home.css";
+import Me from "../../assets/avatar-1.svg"
+import HeaderSocials from './HeaderSocials';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={Me} alt="" className="home__img" />
+        <h1 className="home__name">Daniel Chang</h1>
+        <span className="home__education">I'm an aspiring Frontend Developer</span>
+        <HeaderSocials/>
+      </div>
+    </section>
   )
 }
 
