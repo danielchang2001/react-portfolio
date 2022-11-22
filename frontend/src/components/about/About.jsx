035ebd45c753591fa56e0414ddeee65dd6da4b2d
Daniel Chang <danielchang083@gmail.com>
2022-11-21 20:39:10 -0800

    Add about me img, name, description, short skills bar, styles

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
 import "./about.css";
+import Image from "../../assets/avatar-2.svg"
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className="about container section" id="about">
+      <h2 className="section__title">About Me</h2>
+
+      <div className="about__container grid">
+        <img src={Image} alt="" className="about__img" />
+
+        <div className="about__data grid">
+          <div className="about__info">
+            <p className="about__description">I am Daniel Chang, laisj flijsa dlifjalsi;dlas dijflas idjflai sjd flaijsdl fijals;dif. alsidjfl;asj dfliajsd lif jalsidf</p>
+            <a href="" className="btn">Download Resume</a>
+          </div>
+          <div className="about__skills grid">
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Development</h3>
+                <span className="skills__number">90%</span>
+              </div>
+              <div className="skills__bar">
+                <span className="skills__percentage development"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">UI/UX Design</h3>
+                <span className="skills__number">80%</span>
+              </div>
+              <div className="skills__bar">
+                <span className="skills__percentage ui__design"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Photography</h3>
+                <span className="skills__number">50%</span>
+              </div>
+              <div className="skills__bar">
+                <span className="skills__percentage photography"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
