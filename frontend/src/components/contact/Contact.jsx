f583f287ab5637baccabcebe035719c1b9f3d257
Daniel Chang <danielchang083@gmail.com>
2022-11-27 23:50:46 -0800

    Add emailjs to contact form and new button hover active styling

--- frontend/src/components/contact/Contact.jsx
+++ frontend/src/components/contact/Contact.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useRef }from 'react';
+import {useState} from 'react';
+import emailjs from '@emailjs/browser';
 import './contact.css';
 
 const Contact = () => {
+  const form = useRef();
+  const initialText = "Send message";
+  const [buttonText, setButtonText] = useState(initialText);
+
+  function handleClick() {
+    
+    setButtonText('Message Sent!')
+
+    setTimeout(() => {
+      setButtonText(initialText);
+    }, 3000);
+  }
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_hg2fuan', 'template_i0vtwah', form.current, '9DeAJxMxzgpCb6bIT')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    
+  }
   return (
+    
     <div className="section contact container section" id="contact">
       <h2 className="section__title">Contact Me</h2>
 
@@ -11,23 +36,23 @@ const Contact = () => {
           <h3 className="contact__title">Let's Talk</h3>
           <p className="contact__details">Email: dchang30@ucsc.edu</p>
         </div>
-        <div className="form contact__form">
+        <form ref={form} onSubmit={sendEmail} className="form contact__form">
           <div className="contact__form-group">
             <div className="contact__form-div">
-              <input type="text" className="contact__form-input" placeholder="Your name" />
+              <input type="text" className="contact__form-input" placeholder="Your name" name="user_name"/>
             </div>
             <div className="contact__form-div">
-              <input type="email" className="contact__form-input" placeholder="Your email" />
+              <input type="email" className="contact__form-input" placeholder="Your email" name="user_email"/>
             </div>
           </div>
           <div className="contact__form-div">
-              <input type="text" className="contact__form-input" placeholder="Subject" />
+              <input type="text" className="contact__form-input" placeholder="Subject" name="subject"/>
             </div>
             <div className="contact__form-div contact__form-area">
-              <textarea name="" id="" cols="30" rows="10" className="contact__form-input" placeholder="Your message"></textarea>
+              <textarea name="message" id="" cols="30" rows="10" className="contact__form-input" placeholder="Your message"></textarea>
             </div>
-            <button className="btn">Send Message</button>
-        </div>
+            <button className="btn" type="submit" value="Send" onClick={handleClick}>{buttonText}</button>
+        </form>
       </div>
     </div>
   )
