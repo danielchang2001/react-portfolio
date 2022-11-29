6ad911878bf429d0db3b8343e50d8cdb6c6950e4
Daniel Chang <danielchang083@gmail.com>
2022-11-28 16:21:21 -0800

    Update home and about images

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./home.css";
-import Me from "../../assets/daniel-transformed.png"
+import Me from "../../assets/profile.png"
 import HeaderSocials from './HeaderSocials';
 import ScrollDown from './ScrollDown';
 import Shapes from './Shapes';
